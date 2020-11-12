ee1fe7011ca8b0ea22c75addf6e426bfca72b8f5
christianpazos <70657425+christianpazos@users.noreply.github.com>
2020-11-11 21:51:53 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import app from './app'
 import './database'
 
-app.listen(4000);
 
-console.log('Servidor en puerto' ,4000)
+//app.listen(4000);
+
+//console.log('Servidor en puerto' ,4000)
+
+//server.listen(4000, () => console.log('El servidor esta funcionando'));
+
+const http = require('http');
+const server = http.createServer(app);
+const puerto = app.set('port',process.env.PORT || 4000);
+
+//Server.listen(app.get('port'),() => {
+//   console.log(`server on port ${app.get('port')}`);
+//} );
+
+
+server.listen(app.get(puerto), () => 
+     console.log('El servidor esta funcionando'));
